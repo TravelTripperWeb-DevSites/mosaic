0d648f70fe23ce91d05ff201c0277906415b6e79
dmanikym <dmanikyam@traveltripper.com>
2019-03-27 17:15:40 +0530

    Specials Carousel

--- _jssrc/app.js
+++ _jssrc/app.js
@@ -18,7 +18,6 @@ readyDoc(function() {
 			autoplayTimeout: 3500,
 			autoplayButtonOutput: false,
 			mouseDrag: true,
-			navAsThumbnails: "#custom-nav",
 			controlsContainer: "#reviews-controls",
 		});
 	}
@@ -35,6 +34,26 @@ readyDoc(function() {
 			autoHeight: true,
 			mouseDrag: true,
 			controls: false
+		});
+	}
+
+	if (document.getElementsByClassName('special-offers').length > 0) {
+		// Slider With Images
+		var slider = tns({
+			container: '.special-offers-listings__wrap',
+			items: 1,
+			gutter: 15,
+			speed: 300,
+			mouseDrag: true,
+			edgePadding: 12,
+			nav: false,
+			controlsContainer: "#offers-controls",
+			"responsive": {
+				"768": {
+					gutter: 80,
+					edgePadding: 150,
+				}
+			}
 		});
 	}
 
