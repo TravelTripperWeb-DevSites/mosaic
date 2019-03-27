74137bf1611832817e8ee73f9032968292227e53
dmanikym <dmanikyam@traveltripper.com>
2019-03-27 14:20:08 +0530

    Review slider - Nav yet to be fixed

---
+++ _jssrc/app.js
@@ -0,0 +1,25 @@
+//fallback ready document
+function readyDoc(fn) {
+	var d = document;
+	(d.readyState == 'loading') ? d.addEventListener('DOMContentLoaded', fn): fn();
+}
+
+readyDoc(function() {
+
+	// Slider With Images
+
+	if (document.getElementsByClassName('review-box__wrap').length > 0) {
+		// Slider With Images
+		var slider = tns({
+			container: '.review-box__wrap',
+			speed: 300,
+			controlsContainer: "#reviews-controls",
+			"responsive": {
+				"768": {
+					items: 1
+				}
+			}
+		});
+	}
+
+})
--- _jssrc/main.bundle.js
+++ _jssrc/main.bundle.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+
+//fallback ready document
+function readyDoc(fn) {
+	var d = document;
+	d.readyState == 'loading' ? d.addEventListener('DOMContentLoaded', fn) : fn();
+}
+
+readyDoc(function () {
+
+	// Slider With Images
+
+	if (document.getElementsByClassName('review-box__wrap').length > 0) {
+		// Slider With Images
+		var slider = tns({
+			container: '.review-box__wrap',
+			speed: 300,
+			controlsContainer: "#reviews-controls",
+			"responsive": {
+				"768": {
+					items: 1
+				}
+			}
+		});
+	}
+});
